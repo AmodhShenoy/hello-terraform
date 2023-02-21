ce04791862219baeb976d45beb6b923c20c68253
Amodh Shenoy <amodhshenoy@gmail.com>
2023-02-21 14:35:35 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,15 +3,23 @@ provider "oci" {
   region = "us-ashburn-1"
 }
 
-resource "oci_devops_trigger" "test_trigger" {
+resource "oci_devops_trigger" "test_bitbucket_cloud_trigger" {
+  #Required
+  actions {
     #Required
-    actions {
-        #Required
-        build_pipeline_id = "ocid1.devopsbuildpipeline.oc1.iad.amaaaaaa34lgq7aavylkp2p6upn27jqtuerzfjagkdovobni4n26notpkcua"
-        type = "TRIGGER_BUILD_PIPELINE"
+    build_pipeline_id = "ocid1.devopsbuildpipeline.oc1.iad.amaaaaaa34lgq7aavylkp2p6upn27jqtuerzfjagkdovobni4n26notpkcua"
+    type              = "TRIGGER_BUILD_PIPELINE"
+    #Optional
+    filter {
+      #Required
+      trigger_source = "BITBUCKET_CLOUD"
+      #Optional
+      events = ["PUSH"]
     }
-    project_id = "ocid1.devopsproject.oc1.iad.amaaaaaa34lgq7aanq74w2k4n2kimdcagqrmofpdhpgcbmwyitcfow5s3ssq"
-    trigger_source = "GITHUB"
-
-    display_name = "amodh-tf-trigger"
+  }
+  project_id     = "ocid1.devopsproject.oc1.iad.amaaaaaa34lgq7aanq74w2k4n2kimdcagqrmofpdhpgcbmwyitcfow5s3ssq"
+  trigger_source = "BITBUCKET_CLOUD"
+  #Optional
+  description   = "description"
+  display_name  = "amodh-tf-trigger-yo"
 }
