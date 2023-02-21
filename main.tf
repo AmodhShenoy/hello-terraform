0c25445e89be53b7a5b6ffeeb64d28002f0fc822
Amodh Shenoy <amodhshenoy@gmail.com>
2023-02-21 14:19:17 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,9 +3,15 @@ provider "oci" {
   region = "us-ashburn-1"
 }
 
-resource "oci_objectstorage_bucket" "test_bucket" {
-  #Required
-  compartment_id = "ocid1.compartment.oc1..aaaaaaaavpfmyfqezigrhnbqwp3hnvlqwepgckdl7rgv5mifqhnuktbbvtwq"
-  name = "terraform-test-15Jun"
-  namespace = "axsewmzamo2h"
+resource "oci_devops_trigger" "test_trigger" {
+    #Required
+    actions {
+        #Required
+        build_pipeline_id = "ocid1.devopsbuildpipeline.oc1.iad.amaaaaaa34lgq7aavylkp2p6upn27jqtuerzfjagkdovobni4n26notpkcua"
+        type = "TRIGGER_BUILD_PIPELINE"
+    }
+    project_id = "ocid1.devopsproject.oc1.iad.amaaaaaa34lgq7aanq74w2k4n2kimdcagqrmofpdhpgcbmwyitcfow5s3ssq"
+    trigger_source = "GITHUB"
+
+    display_name = "amodh-tf-trigger"
 }
